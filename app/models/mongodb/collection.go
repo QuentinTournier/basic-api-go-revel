e0af0eaf92d05c438b087018a0c71f2debb84fc3
Etienne DEBARD <etienne.debard@etu.univ-lyon1.fr>
2017-11-08 21:07:08 +0100

    init revel app

---
+++ app/models/mongodb/collection.go
@@ -0,0 +1,32 @@
+package mongodb
+
+import (
+	"gopkg.in/mgo.v2"
+)
+
+type Collection struct {
+	db      *Database
+	name    string
+	Session *mgo.Collection
+}
+
+func (c *Collection) Connect() {
+	session := *c.db.session.C(c.name)
+	c.Session = &session
+}
+
+func NewCollectionSession(name string) *Collection {
+	println(DBNAME)
+	println(name)
+	var c = Collection{
+		db:   newDBSession(DBNAME),
+		name: name,
+	}
+	c.Connect()
+	return &c
+}
+
+func (c *Collection) Close() {
+	service.Close(c)
+}
+
