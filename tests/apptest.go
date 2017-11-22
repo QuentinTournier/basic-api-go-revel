3256961fa80a17eda0f13b1a0416a05df7aa2aff
Adrien COMTE <adrien.comte@etu.univ-lyon1.fr>
2017-11-22 16:45:25 +0100

    Creation test fonctionnels requêtes globales

--- tests/apptest.go
+++ tests/apptest.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"github.com/revel/revel/testing"
+	"io"
 )
 
 type AppTest struct {
@@ -20,4 +21,37 @@ func (t *AppTest) TestThatIndexPageWorks() {
 
 func (t *AppTest) After() {
 	println("Tear down")
+}
+
+/*
+GET /user UserController.Index
+POST /user UserController.Create
+PUT /user UserController.Update
+GET /user/:id UserController.Show
+DELETE /user/:id UserController.Delete
+ */
+
+
+
+func (t *AppTest) TestGetUserRespond(){
+	t.Get("/user")
+	t.AssertContentType("application/json; charset=utf-8")
+	t.AssertOk()
+}
+
+func (t *AppTest) TestPostUserRespond(){
+	var reader io.Reader
+	t.Post("/user","{\"firstName\": \"timeo\",\"lastName\": \"picard\",\"position\": {\"lat\": -40.962026,\"lon\": 24.684415},	\"birthDay\": \"02/24/1994\"}", reader)
+	t.AssertContentType("application/json; charset=utf-8")
+}
+
+func (t *AppTest) TestPutUserRespond(){
+	var reader io.Reader
+	t.Put("/user","{\"firstName\": \"timeo\",\"lastName\": \"picard\",\"position\": {\"lat\": -40.962026,\"lon\": 24.684415},	\"birthDay\": \"02/24/1994\"}", reader)
+	t.AssertContentType("application/json; charset=utf-8")
+}
+
+func (t *AppTest) TestDeleteUserRespond(){
+	t.Delete("/user")
+	t.AssertContentType("text/html; charset=utf-8")
 }
